cec5c118598e6b09ae5313b7465e7b9014bffade
kinzlee <chinemeremnwoga@gmail.com>
2020-06-07 22:34:00 +0100

    first commit

---
+++ screens/CategoriesMealScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+
+const CategoriesMealScreen = () => {
+    <View style={styles.screen}>
+        <Text>categogries screen </Text>
+    </View>
+};
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1
+    }
+});
+
+export default CategoriesMealScreen;
