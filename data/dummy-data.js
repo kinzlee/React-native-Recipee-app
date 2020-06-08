7913dff257d675b6912348423b7d94639b6cffbf
kinzlee <chinemeremnwoga@gmail.com>
2020-06-08 23:47:43 +0100

    fixed up dummy-data

--- data/dummy-data.js
+++ data/dummy-data.js
@@ -3,18 +3,18 @@ import Category from '../models/Category';
 export const CATEGORIES = [
     new Category('c1', 'Nigerian', '#f5428d'),
     new Category('c1', 'Ghanian', '#f5428d'),
-    new Category('c1', 'japanese', '#f5428d'),
-    new Category('c1', 'indonisian', '#f5428d'),
-    new Category('c1', 'algerian', '#f5428d'),
-    new Category('c1', 'italian', '#f5428d'),
-    new Category('c1', 'Nigerian', '#f5428d'),
-    new Category('c1', 'Nigerian', '#f5428d'),
-    new Category('c1', 'Nigerian', '#f5428d'),
-    new Category('c1', 'Nigerian', '#f5428d'),
-    new Category('c1', 'Nigerian', '#f5428d'),
-    new Category('c1', 'Nigerian', '#f5428d'),
-    new Category('c1', 'Nigerian', '#f5428d'),
-    new Category('c1', 'Nigerian', '#f5428d'),
-    new Category('c1', 'Nigerian', '#f5428d')
+    new Category('c1', 'Japanese', '#f5428d'),
+    new Category('c1', 'Indonisian', '#f5428d'),
+    new Category('c1', 'Algerian', '#f5428d'),
+    new Category('c1', 'Italian', '#f5428d'),
+    new Category('c1', 'Russian', '#f5428d'),
+    new Category('c1', 'Korean', '#f5428d'),
+    new Category('c1', 'Chinese', '#f5428d'),
+    new Category('c1', 'south African', '#f5428d'),
+    new Category('c1', 'Egyptian', '#f5428d'),
+    new Category('c1', 'Colombian', '#f5428d'),
+    new Category('c1', 'Mexican', '#f5428d'),
+    new Category('c1', 'Jamaican', '#f5428d'),
+    new Category('c1', 'Lybarian', '#f5428d')
 
 ]
