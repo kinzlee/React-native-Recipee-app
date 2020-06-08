888911b95dbd146f50ebddf6ef3c80ae0f55fa12
kinzlee <chinemeremnwoga@gmail.com>
2020-06-08 23:24:11 +0100

    setting up the dummy-data

---
+++ data/dummy-data.js
@@ -0,0 +1,20 @@
+import Category from '../models/Category';
+
+export const CATEGORIES = [
+    new Category('c1', 'Nigerian', '#f5428d'),
+    new Category('c1', 'Ghanian', '#f5428d'),
+    new Category('c1', 'japanese', '#f5428d'),
+    new Category('c1', 'indonisian', '#f5428d'),
+    new Category('c1', 'algerian', '#f5428d'),
+    new Category('c1', 'italian', '#f5428d'),
+    new Category('c1', 'Nigerian', '#f5428d'),
+    new Category('c1', 'Nigerian', '#f5428d'),
+    new Category('c1', 'Nigerian', '#f5428d'),
+    new Category('c1', 'Nigerian', '#f5428d'),
+    new Category('c1', 'Nigerian', '#f5428d'),
+    new Category('c1', 'Nigerian', '#f5428d'),
+    new Category('c1', 'Nigerian', '#f5428d'),
+    new Category('c1', 'Nigerian', '#f5428d'),
+    new Category('c1', 'Nigerian', '#f5428d')
+
+]
