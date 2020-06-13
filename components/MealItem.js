27adba9eb62719b789d6ecb0b24e7782ab496bee
kinzlee <chinemeremnwoga@gmail.com>
2020-06-13 23:37:43 +0100

    implementing the data for the details screen

--- components/MealItem.js
+++ components/MealItem.js
@@ -1,76 +1,83 @@
-import  React from 'react';
-import {View, Text, StyleSheet, TouchableOpacity, ImageBackground} from 'react-native';
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ImageBackground
+} from "react-native";
+import CustomText from "../components/CustomText";
 
-const MealItem = ({onSelectMeal, title, duration, complexity, affordability, image}) => {
-    return (
-        <View style={styles.mealItem}>
-        <TouchableOpacity onPress={onSelectMeal}>
-            <View>
-            <View style={{...styles.mealRow, ...styles.mealHeader}}>
-                <ImageBackground
-                source={{uri: image}}   
-                style={styles.bgImage}
-                >
-                    <View style={styles.titleContainer}>
-                        <Text 
-                        style={styles.title}
-                        numberOfLines={1}
-                        >
-                        {title}
-                        </Text>
-                    </View>
-                </ImageBackground>
-            </View>
-            <View style={{...styles.mealRow, ...styles.mealDetail}}>
-                <Text>{duration} minutes</Text>
-                <Text>{complexity}</Text>
-                <Text>{affordability}</Text>
-            </View>
-            </View>
-        </TouchableOpacity>
+const MealItem = ({
+  onSelectMeal,
+  title,
+  duration,
+  complexity,
+  affordability,
+  image
+}) => {
+  return (
+    <View style={styles.mealItem}>
+      <TouchableOpacity onPress={onSelectMeal}>
+        <View>
+          <View style={{ ...styles.mealRow, ...styles.mealHeader }}>
+            <ImageBackground source={{ uri: image }} style={styles.bgImage}>
+              <View style={styles.titleContainer}>
+                <Text style={styles.title} numberOfLines={1}>
+                  {title}
+                </Text>
+              </View>
+            </ImageBackground>
+          </View>
+          <View style={{ ...styles.mealRow, ...styles.mealDetail }}>
+            <CustomText>{duration} minutes</CustomText>
+            <CustomText>{complexity.toUpperCase()}</CustomText>
+            <CustomText>{affordability.toUpperCase()}</CustomText>
+          </View>
         </View>
-    );
+      </TouchableOpacity>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    mealItem: {
-        height: 220,
-        width: '100%',
-        backgroundColor: '#ccc',
-        overflow: 'hidden',
-        borderRadius: 10,
-        marginVertical: 10
-        // elevation: 6
-        
-    },
-    mealRow: {
-        flexDirection: "row"
-    },
-    mealDetail: {
-        paddingHorizontal: 10,
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        height: '10%',
-    },
-    mealHeader: {
-        height: '90%'
-    },
-    bgImage: {
-        width: '100%',
-        height: '100%',
-        justifyContent: 'flex-end'
-    },
-    titleContainer: {
-        backgroundColor: 'rgba(0,0,0,0.7)',
-        paddingVertical: 6,
-        paddingHorizontal: 12,
-    },
-    title: {
-        fontFamily: 'Open-Sans-Bold',
-        fontSize: 18,
-        color: '#fff',
-        textAlign: 'center'
-    }
+  mealItem: {
+    height: 220,
+    width: "100%",
+    backgroundColor: "#ccc",
+    overflow: "hidden",
+    borderRadius: 10,
+    marginVertical: 10
+    // elevation: 6
+  },
+  mealRow: {
+    flexDirection: "row"
+  },
+  mealDetail: {
+    paddingHorizontal: 10,
+    justifyContent: "space-between",
+    alignItems: "center",
+    height: "10%"
+  },
+  mealHeader: {
+    height: "90%"
+  },
+  bgImage: {
+    width: "100%",
+    height: "100%",
+    justifyContent: "flex-end"
+  },
+  titleContainer: {
+    backgroundColor: "rgba(0,0,0,0.7)",
+    paddingVertical: 6,
+    paddingHorizontal: 12
+  },
+  title: {
+    fontFamily: "Open-Sans-Bold",
+    fontSize: 18,
+    color: "#fff",
+    textAlign: "center"
+  }
 });
 
 export default MealItem;
--- screens/MealsDetailScreen.js
+++ screens/MealsDetailScreen.js
@@ -1,24 +1,55 @@
-import React from 'react';
-import {View, Text, StyleSheet, Button} from 'react-native';
-import {MEALS} from '../data/dummy-data';
+import React from "react";
+import {
+  ScrollView,
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  Image
+} from "react-native";
+import { MEALS } from "../data/dummy-data";
+import CustomText from "../components/CustomText";
 
-const MealsDetailScreen = ({navigation, route}) => {
-    const {mealId} = route.params;
-    const selectedMeal = MEALS.find(meal => meal.id === mealId)
-    return (
-    <View style={styles.screen}>
-        <Text> {selectedMeal.title} </Text>
-        <Button title="Go Back To Categories" onPress={()=>navigation.popToTop()} />
-    </View>
-    );
+const MealsDetailScreen = ({
+  affordability,
+  duration,
+  complexity,
+  navigation,
+  route
+}) => {
+  const { mealId } = route.params;
+  const selectedMeal = MEALS.find(meal => meal.id === mealId);
+  return (
+    <ScrollView>
+      <Image source={{ uri: selectedMeal.imageUrl }} style={styles.image} />
+      <View style={styles.details}>
+        <CustomText>{selectedMeal.duration} minutes</CustomText>
+        <CustomText>{selectedMeal.complexity.toUpperCase()}</CustomText>
+        <CustomText>{selectedMeal.affordability.toUpperCase()}</CustomText>
+      </View>
+      <Text style={styles.title}>Ingredients</Text>
+      <Text>List Of Ingredients...</Text>
+      <Text style={styles.title}>Steps</Text>
+      <Text>List Of Steps</Text>
+    </ScrollView>
+  );
 };
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  image: {
+    width: "100%",
+    height: 220
+  },
+  details: {
+    padding: 15,
+    flexDirection: "row",
+    justifyContent: "space-between"
+  }
 });
 
 export default MealsDetailScreen;
