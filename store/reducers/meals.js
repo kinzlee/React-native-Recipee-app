10676e0b5194c8c2f93af420cbf3cee7a9cbb9b4
kinzlee <chinemeremnwoga@gmail.com>
2020-06-19 00:08:27 +0100

    adding favourite

--- store/reducers/meals.js
+++ store/reducers/meals.js
@@ -1,4 +1,5 @@
 import { MEALS } from "../../data/dummy-data";
+import { TOGGLE_FAVOURITE } from "../actions/meals";
 
 const initialState = {
   meals: MEALS,
@@ -7,6 +8,23 @@ const initialState = {
 };
 
 const mealsReducer = (state = initialState, action) => {
-  return state;
+  switch (action.type) {
+    case TOGGLE_FAVOURITE:
+      const existingIndex = state.favouriteMeals.findIndex(
+        meal => meal.id === mealId
+      );
+
+      if (existingIndex >= 0) {
+        const updatedFavMeal = [...state.favouriteMeals];
+        updatedFavMeal.splice(existingIndex, 1);
+        return { ...state, favouriteMeals: updatedFavMeal };
+      } else {
+        meals = state.favouriteMeals.find(meal => mealid === mealId);
+        return { ...state, favouriteMeals: state.favouriteMeals.concat(meals) };
+      }
+
+    default:
+      return state;
+  }
 };
 export default mealsReducer;
